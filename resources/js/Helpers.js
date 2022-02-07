78a8cebf22f4e3f3fe958f6777c6a1f3256aed47
ghanshyam <ghanshyam.crawlapps@gmail.com>
2022-02-07 15:29:25 +0530

    inital

---
+++ resources/js/Helpers.js
@@ -0,0 +1,16 @@
+
+export function showSznNotification(params) {
+    return iziToast.show({
+        title: params.title ? params.title : '',
+        message: params.message,
+        messageSize: 12,
+        position: 'topRight',
+        theme: 'dark',
+        pauseOnHover: true,
+        color: params.type == 'success' ? '#565c70' : '#565c70',
+        messageColor: params.type == 'success' ? '#00ffb8' : '#ffafb4',
+        icon: params.type == 'success' ? 'mdi mdi-check' : 'mdi mdi-alert-circle-outline'
+    });
+}
+
+
